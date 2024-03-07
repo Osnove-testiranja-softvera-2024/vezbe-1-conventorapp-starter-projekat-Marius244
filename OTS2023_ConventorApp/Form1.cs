fe080012b8012ceb44a467ccee3bd04292be7c4f
marius244 <sstefan.marius24@gmail.com>
2024-03-07 19:33:30 +0100

    prvi zadatak

--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,53 @@ namespace OTS2023_ConventorApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double input = Convert.ToDouble(textBox1.Text);
+            double result;
+
+            if (radioButton1.Checked)
+            {
+                result = input * 0.453592; 
+            }
+            else
+            {
+                result = input * 0.3048; 
+            }
+
+            textBox2.Text = (result.ToString());
         }
     }
 }
